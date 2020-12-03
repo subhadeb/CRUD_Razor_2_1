b28d5613084dbf07b3d0c18f6fd8e36b9046cca7
Subha Deb <subdeb@deloitte.com>
2020-12-03 08:48:38 +0530

    After 243 Edit Page Model,  After Section 22 ARCHIVE Project 1  CRUD Operation Book Razor Pages Ends

---
+++ CRUD_Razor_2_1/CRUD_Razor_2_1/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUD_Razor_2_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRUD_Razor_2_1.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        public void OnGet(int id)
+        {
+            Book = _db.Books.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var BookFromDb = _db.Books.Find(Book.Id);
+                BookFromDb.Name = Book.Name;
+                BookFromDb.ISBN = Book.ISBN;
+                BookFromDb.Author = Book.Author;
+
+                await _db.SaveChangesAsync();
+                Message = "Book has been updated successfully";
+
+                return RedirectToPage("Index");
+            }
+
+            return RedirectToPage();
+        }
+    }
+}
